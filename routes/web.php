ea68bc9ed1b4176f4d84bf9c7ca1da363df51fb3
Karine Cardozo <karinecardozo@ejcm.com.br>
2019-04-11 16:35:03 -0300

    Aprendendo rotas

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,33 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::prefix('app')->group(function(){
+    Route::get("/", function(){
+        return "<h1>Página Main</h1>";
+    });
+    
+    Route::get("profile", function(){
+        return "<h1>Página do Perfil</h1>";
+    });
+    
+    Route::get("about", function(){
+        return "<h1>Página Sobre</h1>";
+    });
+});
+Route::get('/olar', function(){
+    return "Olar";
+});
+
+Route::redirect('/aqui', '/olar', 301);
+
+//Passando parametros na view e rotas
+Route::view('/hello', 'hello');
+Route::view('/viewnome', 'hellonome', 
+            ['nome'=>'Karine',
+             'sobrenome'=>'Cardozo']
+);
+Route::get('/hellonome/{nome}/{sobrenome}', function($nome,$sn){
+    return view('hellonome', ['nome'=>$nome,'sobrenome'=>$sn]);
+});
